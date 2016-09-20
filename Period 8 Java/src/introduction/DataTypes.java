a89bb9bc349eb774152a17def735a110b2e96da9
Student8 <Student8@10.8.33.167>
2016-09-20 13:47:59 -0400

    9/20/16

---
+++ Period 8 Java/src/introduction/DataTypes.java
@@ -0,0 +1,13 @@
+package introduction;
+
+public class DataTypes {
+	
+	public static void main(String[] args) {
+		String ten = "10";
+		String two = "two";
+		if (ten.compareTo(two)>0) System.out.println("10 is greater than 2)");
+			else System.out.println("10 is not greater than 2");
+		
+	}
+
+}
---
+++ Period 8 Java/src/introduction/RandomDiceRoll.java
@@ -0,0 +1,42 @@
+package introduction;
+
+public class RandomDiceRoll {
+
+	public static void main(String[] args) {
+		
+		int[] results = new int[6];
+		
+		
+		for(int index = 0; index < 1000; index++){
+			int result = rollUnfairDie();
+			System.out.println("Roll #"+ (index+1)+ ": " +result);
+			results[result-1]++;
+			
+			}
+		
+		for(int i = 0; i<6; i++){
+			System.out.println((i+1)+" appeared " + results[i] + " times.");
+		}
+		
+	}
+	
+	
+	
+	//return 1,2,3,4,5,6 with equal probability
+	public static int rollFairDie(){
+		double rand = Math.random();//returns a double (0,1)
+		int roll = (int)(6*rand);//[0,5]
+		roll = roll+1;
+		return roll;
+	}
+	
+	public static int rollUnfairDie(){
+		double random1 = Math.random();
+		double random2 = Math.random();
+		int roll = (int)(3*(random1+random2));
+		roll = roll+1;
+		return roll;
+	}
+	
+	
+}
