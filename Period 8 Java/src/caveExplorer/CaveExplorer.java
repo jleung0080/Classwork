21f4b5aaeeec094b8103db4c9320b5e3dbb43bcb
Student8 <Student8@10.8.33.167>
2016-10-31 14:01:55 -0400

    a

---
+++ Period 8 Java/src/caveExplorer/CaveExplorer.java
@@ -0,0 +1,27 @@
+package caveExplorer;
+
+import java.util.Scanner;
+
+public class CaveExplorer {
+	public static CaveRoomJoey[][] caves;
+	public static Scanner in;
+	public static CaveRoomJoey currentRoom;
+	public static InventoryNockles inventory;
+	
+	
+	
+	public static void main(String[] args){
+		in = new Scanner(System.in);
+		caves = new CaveRoomJoey[5][5];
+		for(int row = 0; row<caves.length; row++){
+			for(int col = 0; col<caves[row].length; col++){
+				caves[row][col] = new CaveRoomJoey("This room has coordinates "+row+", " +col);
+			}
+		}
+		currentRoom = caves[1][2];
+		currentRoom.enter();
+		caves[1][2].setConnection(caves[1][1]);
+		caves[1][2].setConnection(caves[1][2]);
+		caves[1][2].setConnection(caves[1][3]);
+	}
+}
---
+++ Period 8 Java/src/caveExplorer/CaveRoomJoey.java
@@ -0,0 +1,17 @@
+package caveExplorer;
+
+public class CaveRoomJoey {
+
+	public CaveRoomJoey(String description) {
+		
+	}
+
+	public void enter() {
+		
+	}
+
+	public void setConnection(CaveRoomJoey caveRoomJoey) {
+		
+	}
+
+}
