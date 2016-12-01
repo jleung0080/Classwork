1b27ed06bde2ae0dbf4ed2b4ddcfd21aad6ae7c6
Student8 <Student8@10.8.33.167>
2016-12-01 14:02:37 -0500

    a

--- Period 8 Java/src/sort/RecursionIntro.java
+++ Period 8 Java/src/sort/RecursionIntro.java
@@ -4,6 +4,8 @@ public class RecursionIntro {
 
 
 	public static void main(String[] args){
+		int n = 100;
+		System.out.println("The " + n + "th Fibonacci is "+fibonacci(n));
 //		System.out.println("Using a for loop:");
 //		for(int i = 0; i<5; i++){
 //			System.out.println("Hello world!");
@@ -22,7 +24,17 @@ public class RecursionIntro {
 //		});
 		
 		
-		hanoiSolution(4, "A", "B", "C");
+//		hanoiSolution(4, "A", "B", "C");
+	}
+
+	private static int fibonacci(int n) {
+		if(n<=1){
+			return 1;
+		}else{
+			int previous = fibonacci(n-1);
+			int beforePrevious = fibonacci(n-2);
+			return previous+beforePrevious;
+		}
 	}
 
 	private static void forLoop(int i, Action action){
@@ -43,13 +55,20 @@ public class RecursionIntro {
 	
 	public static void hanoiSolution(int numberOfDiscs, String startPeg, String midPeg, String endPeg){
 		if(numberOfDiscs<=1){
-			System.out.println("Move " +startPeg+" to "+endPeg);
+			print("Move " +startPeg+" to "+endPeg);
 		}else{
+			System.out.println("In order to move "+numberOfDiscs+" over to peg "+endPeg+", we must move"
+					+ " "+ (numberOfDiscs-1) +" over to peg "+midPeg+" first");
 			hanoiSolution(numberOfDiscs-1, startPeg, endPeg, midPeg);
 			hanoiSolution(1, startPeg, midPeg, endPeg);
 			hanoiSolution(numberOfDiscs-1, midPeg, startPeg, endPeg);
 		}
 	}
 	
+	private static int count = 1;
 	
+	public static void print(String s){
+		System.out.println("Move #"+count+":"+s);
+		count++;
+	}
 }
