2b0b9dc4881d16d895b1098c87b7e376003c706f
Student8 <Student8@10.8.33.167>
2016-12-06 14:03:29 -0500

    a

--- Period 8 Java/src/sort/Sort.java
+++ Period 8 Java/src/sort/Sort.java
@@ -6,19 +6,19 @@ public class Sort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		int[] arrayToSort={133,0,-5,31,12,12,2};
-//		System.out.println("- - - SELECTION SORT - - -");
-//		System.out.println(Arrays.toString(arrayToSort));
-//		selectionSort(copy(arrayToSort));
-		
+		//		int[] arrayToSort={133,0,-5,31,12,12,2};
+		//		System.out.println("- - - SELECTION SORT - - -");
+		//		System.out.println(Arrays.toString(arrayToSort));
+		//		selectionSort(copy(arrayToSort));
+
 		int[] arrayToSort={133,0,-5,31,12,12,2};
 		int[] sorted = mergeSort(arrayToSort);
 		for(int a:sorted){
 			System.out.println(a);
 		}
-		
+
 	}
-	
+
 	private static int[] mergeSort(int[] list){
 		if(list.length==1){
 			return list;	
@@ -37,23 +37,37 @@ public class Sort {
 
 	private static int[] merge(int[] list1, int[] list2) {
 		// TODO Auto-generated method stub
-		int[] list = new int[list1.length+list2.length];
-		int counter = 0;
-		for(int i = 0; i < list1.length;i++){
-			for(int j = 0; j < list2.length;j++){
-				if(list1[i]<list2[j]){
-					list[counter]=list1[i];
-					counter++;
-					break;
-				}else{
-					list[counter]=list2[j];
-					counter++;
+		if(list1.length<1){
+			return list2;
+		}else if(list2.length<1){
+			return list1;
+		}else{
+
+			int[] list = new int[list1.length+list2.length];
+			int counter = 0;
+			if(list1[0]<list2[0]){
+				list[0] = list1[0];
+				int[] newList = new int[list1.length-1];
+				for(int i = 1; i<list1.length;i++){
+					newList[i-1] = list1[i];
+				}
+				for(int i = 1; i<list.length;i++){
+					list[i]= merge(newList,list2)[i-1];
+				}
+			}else{
+				list[0] = list2[0];
+				int[] newList = new int[list2.length-1];
+				for(int i = 1; i<list2.length;i++){
+					newList[i-1] = list2[i];
+				}
+				for(int i = 1; i<list.length;i++){
+					list[i]= merge(list1,newList)[i-1];
 				}
 			}
+			return list;
 		}
-		return list;
 	}
-	
+
 	public static int[] copy(int[] arr){
 		int[] copy = new int[arr.length];
 		for(int i = 0; i < copy.length; i++){
@@ -61,7 +75,7 @@ public class Sort {
 		}
 		return copy;
 	}
-	
+
 	public static void swap(int[] arr, int i, int j){
 		System.out.println("Swapping "+arr[i]+" and "+arr[j]);
 		int placeHolder=arr[j];
@@ -69,7 +83,7 @@ public class Sort {
 		arr[i]=placeHolder;
 		System.out.println(Arrays.toString(arr));
 	}
-	
+
 	public static void selectionSort(int[] arrayToSort){
 		int minIndex=0;
 		int minimum=arrayToSort[0];
