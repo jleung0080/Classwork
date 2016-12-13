93cda432beea0bfc72f8785be7709e32a36e4060
Student8 <Student8@10.8.33.167>
2016-12-13 13:56:38 -0500

    a

--- Period 8 Java/src/guiPractice/Screen.java
+++ Period 8 Java/src/guiPractice/Screen.java
@@ -37,6 +37,9 @@ public abstract class Screen {
 	public void update() {
 		Graphics2D g = image.createGraphics();
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+		
+		g.setColor(Color.white);
+		g.fillRect(0, 0, image.getWidth(),image.getHeight());
 		g.setColor(Color.black);
 		//draw all visible components
 		for(Visible v: viewObjects){
